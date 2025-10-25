9f5d2e35f62668dfcbe3df71759cc7b58e4ca61f
Jonathan <jonathan.a.farias@gmail.com>
2025-10-25 09:32:03 -0300

    feat: exercises completed

---
+++ aula-02/exercicios/cemAnos.js
@@ -0,0 +1,3 @@
+idade = 2
+meta = 100
+console.log(`Faltam ${meta - idade} anos para você alcançar a meta  de ${meta} anos de idade`)
---
+++ aula-02/exercicios/operacaoAritmetica.js
@@ -0,0 +1,5 @@
+// Crie uma variável numero e mostre o dobro e a metade desse número.
+numero1 = 20
+console.log(`O número é ${numero1}`)
+console.log(`O dobro do número é ${numero1 * 2}`)
+console.log(`A Metade de número é ${numero1 / 2}`)

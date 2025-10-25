c17fdc9201e360c965a7ba612561a0d865440a77
Jonathan <jonathan.a.farias@gmail.com>
2025-10-25 09:08:21 -0300

    feat: recapitulation of concepts

---
+++ aula-02/revisao.js
@@ -0,0 +1,25 @@
+// Variáveis
+let nome = "Jonathan"
+let idade = 20
+const habilidade = true
+
+// Estrutura de Dados
+let frutas = ["Maça","Uva","Morango"]
+let nomes = ["Jonathan","Will","Matheus"]
+
+console.log(frutas)
+console.log(frutas[0])
+console.log(frutas[1])
+console.log(frutas[2])
+
+// Operadores aritméticos 
+
+let numero1 = 20
+let numero2 = 21
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a**b)
+console.log(a/b)
+console.log(a%b)

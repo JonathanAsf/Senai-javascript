42089e6edef752f83cee4bd5c7648436aeecbf2c
Jonathan <jonathan.a.farias@gmail.com>
2025-10-25 16:35:39 -0300

    feat: control structure

---
+++ aula-02/estruturaControle/estruturaCondicional.js
@@ -0,0 +1,80 @@
+// let sinal = "amarelo"
+
+// if(sinal == "verde"){
+//     console.log("Avance")
+// }else if(sinal == "amarelo"){
+//     console.log("Atenção")
+// }else{
+//     console.log("Aguarde")
+// }
+
+// let dia = 5
+
+// switch(dia){
+//     case 1:
+//         console.log("Domingo")
+//         break
+//     case 2:
+//         console.log("Segunda-feira")
+//         break
+//     case 3:
+//         console.log("Terça-feira")
+//         break
+//     case 4:
+//         console.log("Quarta-feira")
+//         break
+//     case 5:
+//         console.log("Quinta-feira")
+//         break
+//     case 6:
+//         console.log("Sexta-feira")
+//         break
+//     case 7:
+//         console.log("Sábado")
+//         break
+//     default:
+//         console.log("Opção inválida.")
+// }
+
+let mes = 2
+
+switch(mes){
+    case 1:
+        console.log("Janeiro")
+        break
+    case 2:
+        console.log("Fevereiro")
+        break
+    case 3:
+        console.log("Março")
+        break
+    case 4:
+        console.log("Abril")
+        break
+    case 5:
+        console.log("Maio")
+        break
+    case 6:
+        console.log("Junho")
+        break
+    case 7:
+        console.log("Julho")
+        break
+    case 8:
+        console.log("Agosto")
+        break
+    case 9:
+        console.log("Setembro")
+        break
+    case 10:
+        console.log("Outubro")
+        break
+    case 11:
+        console.log("Novembro")
+        break
+    case 12:
+        console.log("Dezembro")
+        break
+    default:
+        console.log("Opção inválida.")
+}

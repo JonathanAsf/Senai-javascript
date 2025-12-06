e9abea0536ba8382da25f78d22fbba63cfba7d1d
Jonathan <jonathan.a.farias@gmail.com>
2025-12-06 11:26:48 -0300

    feat: example using forEach

--- aula-06/forEach.js
+++ aula-06/forEach.js
@@ -3,10 +3,13 @@ let nomes = [
   "Gabriel", "Isabela", "Ricardo", "Camila", "Felipe", "Larissa", "Marcos", "Amanda", 
 ]
 
+let idades = [23, 34, 45, 19, 29, 56, 61, 40, 17, 50]
+
+
 nomes.forEach((elemento) => {
     console.log (`Olá, ${elemento}`);
 })
 
-// nomes.forEach(function(elemento){
-//     console.log (`Olá, ${elemento}`);
-// })
+idades.forEach(function(elemento){
+     console.log (`Olá, ${elemento}`);
+})

fcd391f643fdc7a90ddfd5b7b1c00a49cde423c2
Jonathan <jonathan.a.farias@gmail.com>
2025-12-06 09:57:35 -0300

    feat: adding forEach example

---
+++ aula-06/forEach.js
@@ -0,0 +1,8 @@
+let nomes = [
+  "Jonathan", "Pedro", "Maria", "João", "Carlos", "Fernanda", "Lucas", "Juliana", "Paula", "Rafael",
+  "Gabriel", "Isabela", "Ricardo", "Camila", "Felipe", "Larissa", "Marcos", "Amanda", 
+]
+
+nomes.forEach(function(elemento){
+    console.log (`Olá, ${elemento}`);
+})
--- aula-07/rickAndMorty/script.js
+++ aula-07/rickAndMorty/script.js
@@ -2,5 +2,7 @@ fetch('https://rickandmortyapi.com/api/character')
 
 .then((res)=> res.json())
 .then((data) =>{
-    console.log(data)
+    console.log(data.results)
 })
+
+.catch((err)=> console.log(err))

8d719cf38ea5342d27976e09b63f9d9e3cd1de41
Jonathan <jonathan.a.farias@gmail.com>
2025-12-06 11:27:13 -0300

    feat: project consuming rick&morty API

--- aula-07/rickAndMorty/script.js
+++ aula-07/rickAndMorty/script.js
@@ -1,8 +1,19 @@
+const container = document.getElementById("container")
+
 fetch('https://rickandmortyapi.com/api/character')
+    .then((res)=> res.json())
+    .then((data) =>{
+        console.log(data.results)
 
-.then((res)=> res.json())
-.then((data) =>{
-    console.log(data.results)
-})
-
-.catch((err)=> console.log(err))
+        data.results.forEach((elemento) => {
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${elemento.image}">
+                <h2>${elemento.name}</h2>
+                <p>${elemento.species}</p>
+            `
+            container.appendChild(card)
+            })
+    })
+    .catch((err)=> console.log(`${err}`))

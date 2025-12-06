de77cf7a0a1d59cf23cdb71232f418c976ff9b34
Jonathan <jonathan.a.farias@gmail.com>
2025-12-06 16:50:23 -0300

    feat: dragon ball API consumed

--- aula-07/dragonBall/script.js
+++ aula-07/dragonBall/script.js
@@ -1,21 +1,21 @@
-const container = document.getElementById("personagens-container")
+    const container = document.getElementById("personagens-container")
 
-fetch('https://dragonball-api.com/api/characters')
-    .then((res)=> res.json())
-    .then((data) =>{
-        console.log(data.items)
+    fetch('https://dragonball-api.com/api/characters')
+        .then((res)=> res.json())
+        .then((data) =>{
+            console.log(data.items)
 
-        data.items.forEach((elemento) => {
-            const card = document.createElement("div")
-            card.className = "card"
-            card.innerHTML = `
-                <img src="${elemento.image}">
-                <h2>${elemento.name}</h2>
-                <p>${elemento.race}</p>
-                <p>G${elemento.gender}</p>
+            data.items.forEach((elemento) => {
+                const card = document.createElement("div")
+                card.className = "card"
+                card.innerHTML = `
+                    <img src="${elemento.image}">
+                    <h2>${elemento.name}</h2>
+                    <p>${elemento.race}</p>
+                    <p>Ki: ${elemento.ki}</p>
 
-            `
-            container.appendChild(card)
-            })
-    })
-    .catch((err)=> console.log(`${err}`))
+                `
+                container.appendChild(card)
+                })
+        })
+        .catch((err)=> console.log(`${err}`))
